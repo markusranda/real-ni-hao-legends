58067dd1dc2e72367ce43eae7f311f013e4fb30c
markusranda <randamarkus.95@gmail.com>
2024-03-26 18:46:09 +0100

    Added neede validation feedback

--- front-end/src/components/TheIntroduction.vue
+++ front-end/src/components/TheIntroduction.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="the-introduction">
     <h1>Who are you today?</h1>
-    <div class="d-flex gap-1">
-      <input v-model="townName" type="text" />
-      <Button @click="handleClickSubmit">Submit</Button>
+    <div class="the-introduction-grid">
+      <div class="d-flex gap-1 w-100 justify-content-center">
+        <input v-model="townName" name="name" type="text" />
+        <Button @click="handleClickSubmit">Submit</Button>
+      </div>
+
+      <p class="error-text" v-if="error">
+        Name must be between 2 to 25 characters long, <br />
+        and contain no special characters
+      </p>
     </div>
   </div>
 </template>
@@ -14,17 +21,21 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      townName: ''
+      townName: '',
+      error: false
     }
   },
   emits: ['done'],
   methods: {
     handleClickSubmit() {
-      const regex = /^[a-zA-Z0-9 ]{4,19}$/
+      const regex = /^[a-zA-Z0-9 ]{2,25}$/
       const valid = regex.test(this.townName)
 
       if (valid) {
+        this.error = false
         this.$emit('done', this.townName)
+      } else {
+        this.error = true
       }
     }
   }
@@ -32,6 +43,11 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.error-text {
+  text-align: center;
+  color: rgb(233, 60, 60);
+}
+
 .the-introduction {
   display: flex;
   flex-direction: column;
@@ -42,5 +58,14 @@ export default defineComponent({
   width: 100vw;
 
   padding: 4rem;
+}
+
+.the-introduction-grid {
+  display: grid;
+  justify-content: center;
+  gap: 2rem;
+  grid-template-rows: auto 4rem;
+
+  width: 100%;
 }
 </style>
